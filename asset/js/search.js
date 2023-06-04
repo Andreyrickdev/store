f683fe036b64d015333d0f821109a97436322750
andreyrickdev <contato-andrey@outlook.com>
2023-06-04 16:02:43 -0300

    adjusted id

--- asset/js/search.js
+++ asset/js/search.js
@@ -1,6 +1,6 @@
 function functSearch () {
 
-    const search = document.getElementById('search');
+    const search = document.getElementById('search-img');
     const inputSearch = document.getElementById('search-now');
 
     search.addEventListener('click', () => {
