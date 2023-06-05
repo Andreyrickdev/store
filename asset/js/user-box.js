aa6bb81435d91c96f630b0c3144184fa3ab7543f
andreyrickdev <contato-andrey@outlook.com>
2023-06-05 19:22:51 -0300

    created event listener when event click start, box add or remove class "active"

--- asset/js/user-box.js
+++ asset/js/user-box.js
@@ -3,6 +3,10 @@ function userBox () {
     const button = document.getElementById('user');
     const box = document.getElementById('user-box');
 
+    button.addEventListener('click', () => {
+        box.classList.toggle('active');
+    });
+
 }
 
 userBox();
