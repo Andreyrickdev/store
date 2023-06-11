8c7f953466a27f8ac4f810b7f6cdef14b20aba9c
andreyrickdev <contato-andrey@outlook.com>
2023-06-11 11:26:44 -0300

    get elements box

--- asset/js/search.js
+++ asset/js/search.js
@@ -2,6 +2,7 @@ function functSearch () {
 
     const search = document.getElementById('search');
     const inputSearch = document.getElementById('search-now');
+    const boxs = document.getElementsByClassName('boxs');
 
     search.addEventListener('click', () => {
         inputSearch.classList.toggle('active');
