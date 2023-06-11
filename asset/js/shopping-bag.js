c33703c6193a1585c65b8d2611d02bebc34a0f48
andreyrickdev <contato-andrey@outlook.com>
2023-06-11 10:54:18 -0300

    get box

--- asset/js/shopping-bag.js
+++ asset/js/shopping-bag.js
@@ -2,7 +2,7 @@ function shoppingBag () {
 
     const buttonOpenAndClose = document.getElementById('shopping-bag');
     const buttonClose = document.getElementById('close-button');
-
+    const box = document.getElementById('shopping-bag-box');
 
 }
 
