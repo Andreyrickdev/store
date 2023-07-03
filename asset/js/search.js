a2b4ace551b5c42c11cf5f7c76b569c5aad66aff
andreyrickdev <contato-andrey@outlook.com>
2023-07-03 19:04:38 -0300

    added new get

--- asset/js/search.js
+++ asset/js/search.js
@@ -2,8 +2,9 @@ function functSearch () {
 
     const search = document.getElementById('search');
     const inputSearch = document.getElementById('search-now');
-    
+
     const shoppingBagBox = document.getElementById('shopping-bag-box');
+    const userBox = document.getElementById('user-box');
 
 
     search.addEventListener('click', () => {
