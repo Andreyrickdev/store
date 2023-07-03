c55366912f2715e2bc378e6aca3f34c728075a49
andreyrickdev <contato-andrey@outlook.com>
2023-07-03 19:03:01 -0300

    only spaces

--- asset/js/search.js
+++ asset/js/search.js
@@ -2,6 +2,7 @@ function functSearch () {
 
     const search = document.getElementById('search');
     const inputSearch = document.getElementById('search-now');
+    
     const shoppingBagBox = document.getElementById('shopping-bag-box');
 
 
