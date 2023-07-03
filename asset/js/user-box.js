574b246b7abb4422e1e4d974b37fa7d882fdd940
andreyrickdev <contato-andrey@outlook.com>
2023-07-03 19:11:21 -0300

    get two boxs

--- asset/js/user-box.js
+++ asset/js/user-box.js
@@ -3,6 +3,9 @@ function userBox () {
     const button = document.getElementById('user');
     const box = document.getElementById('user-box');
 
+    const searchBox = document.getElementById('search-now');
+    const shoppingBagBox = document.getElementById('shopping-bag-box');
+
     button.addEventListener('click', () => {
         box.classList.toggle('active');
     });
