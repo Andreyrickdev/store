671b746eede7b1b5b931f5f49bb1f02772c8fc6e
andreyrickdev <contato-andrey@outlook.com>
2023-07-05 05:33:08 -0300

    get element

--- asset/js/putBag.js
+++ asset/js/putBag.js
@@ -1,6 +1,7 @@
 function putBag () {
 
     const items = document.getElementsByClassName('item-food');
+    const bag = document.getElementById('items-in-bag');
 
     for(let item of items) {
         item.addEventListener('click', () => {
