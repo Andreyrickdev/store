fe5a8f924e130e69054d7c197c3f0b949dbd1a6c
andreyrickdev <contato-andrey@outlook.com>
2023-07-05 05:31:06 -0300

    created structure loop and add Event Listener on item

--- asset/js/putBag.js
+++ asset/js/putBag.js
@@ -2,6 +2,11 @@ function putBag () {
 
     const items = document.getElementsByClassName('item-food');
 
+    for(let item of items) {
+        item.addEventListener('click', () => {
+            
+        })
+    }
 }
 
 putBag();
