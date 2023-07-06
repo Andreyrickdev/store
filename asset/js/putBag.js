91ee17bfda186ac7a0c4b48deecc93be4966fe47
andreyrickdev <contato-andrey@outlook.com>
2023-07-06 19:42:34 -0300

    add in div

--- asset/js/putBag.js
+++ asset/js/putBag.js
@@ -52,6 +52,7 @@ function putBag () {
 
                     div.appendChild(remove);
                     div.appendChild(paragraph);
+                    div.appendChild(add);
                 }
             }
 
