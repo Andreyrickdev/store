bafb98292163785195c09031f79dedd590509904
andreyrickdev <contato-andrey@outlook.com>
2023-07-17 19:13:45 -0300

    adjusted all for

--- asset/js/putBag.js
+++ asset/js/putBag.js
@@ -5,8 +5,8 @@ function putBag () {
     const items = document.getElementsByClassName('item-food');
     const bag = document.getElementById('items-in-bag');
 
-    for(let item of items) {
-        item.addEventListener('click', () => {
+    for( let item = 0; item < items.length; item++) {
+        items[item].addEventListener('click', () => {
 
             function removePhraseEmpty () {
 
