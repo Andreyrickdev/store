81376ffa3eec0ceaaa58fe32bb7370bd50b30ceb
andreyrickdev <contato-andrey@outlook.com>
2023-07-17 18:24:19 -0300

    get id

--- asset/js/putBag.js
+++ asset/js/putBag.js
@@ -10,7 +10,7 @@ function putBag () {
 
             function removePhraseEmpty () {
 
-                
+                const phrase = document.getElementById('cart-empty');
 
             }
             
