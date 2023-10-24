6b9b760e36fa36d3e93d8153d91acee93847e51c
Andreyrickdev <contato-andrey@outlook.com>
2023-10-24 17:33:19 -0300

    created function and added heart in this function, and created addEventListener for loop structure

--- asset/js/favoriteOrNo.js
+++ asset/js/favoriteOrNo.js
@@ -2,3 +2,16 @@ function hearts () {
     const heart = document.getElementsByClassName('favorite');
     return heart;
 }
+
+function eventOnHeart () {
+
+    const heart = hearts();
+
+    for (let fav of heart) {
+        fav.addEventListener('click', () => {
+            
+        });
+    }
+}
+
+eventOnHeart();
