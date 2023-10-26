374453940b816a5855848d26d7cd74adf56a3d4d
Andreyrickdev <contato-andrey@outlook.com>
2023-10-26 19:06:06 -0300

    removed, because is not necessary

--- others-pages/bag/others-script/addOrRemoveItems.js
+++ others-pages/bag/others-script/addOrRemoveItems.js
@@ -1,10 +1,5 @@
 function addOrRemoveItems () {
 
-    function selectedNumItems () {
-        const num = document.getElementsByClassName('num-items');
-        return num;
-    }
-
     function addOrRemove () {
 
         document.addEventListener('click', (e) => {
