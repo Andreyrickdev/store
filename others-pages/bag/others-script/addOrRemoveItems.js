e9c14e7728c49f943048659b76a6dc9ba78a2bc9
Andreyrickdev <contato-andrey@outlook.com>
2023-10-26 19:22:09 -0300

    get dad-div and get p

--- others-pages/bag/others-script/addOrRemoveItems.js
+++ others-pages/bag/others-script/addOrRemoveItems.js
@@ -5,6 +5,8 @@ function addOrRemoveItems () {
         document.addEventListener('click', (e) => {
             
             const buttonAddOrRemove = e.target; 
+            const getDadDiv = buttonAddOrRemove.parentNode;
+            const getNumItem = getDadDiv.querySelector('p');
             
             if(buttonAddOrRemove.classList.contains('button-remove')) {
                 alert('deu certo');
