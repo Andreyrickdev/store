5be684079ff45e9d97814721257c6074c05e299f
Andreyrickdev <contato-andrey@outlook.com>
2023-11-01 19:45:27 -0300

    created event for button

--- others-pages/bag/others-script/addPrice.js
+++ others-pages/bag/others-script/addPrice.js
@@ -3,6 +3,9 @@ function addPrice () {
     const button = document.getElementById('footer');
     const numPrice = button.getElementsByClassName('num-prices')[0];
 
+    button.addEventListener('click', () => {
+
+    });
 
 }
 
