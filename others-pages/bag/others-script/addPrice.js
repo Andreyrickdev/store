f3f32ed2b087d7f0eadf1310b387228c33879811
Andreyrickdev <contato-andrey@outlook.com>
2023-11-02 17:29:29 -0300

    adjusted event listener and created const

--- others-pages/bag/others-script/addPrice.js
+++ others-pages/bag/others-script/addPrice.js
@@ -3,35 +3,10 @@ function addPrice () {
     const button = document.getElementById('footer');
     const numPrice = button.getElementsByClassName('num-prices')[0];
 
-    button.addEventListener('click', () => {
+    document.addEventListener('click', (e) => {
 
-        function selectItems () {
-            const items = document.getElementsByClassName('product');
-            return items;
-        }
+        const clickedOnButtons = e.target;
 
-        let sumAllMultiplications = [];
-        let sum;
-
-        function readAllItems () {
-
-            const items = selectItems();
-
-            for(let item of items) {
-                const allH1 = item.querySelector('h1').innerText;
-                const allNumItems = item.querySelector('.num-items').innerText;
-                const resultMultiplications = allH1 * allNumItems;
-
-                sumAllMultiplications.push(resultMultiplications);
-
-                sum = sumAllMultiplications.reduce((total, valor) => total + valor, 0);
-            }
-
-            numPrice.innerText = sum;
-
-        }
-
-        readAllItems();
 
     });
 
