427d08d445a6e9693a52cb00b4214a46c9c05720
Andreyrickdev <contato-andrey@outlook.com>
2023-11-04 16:05:22 -0300

    adjusted name

--- others-pages/bag/others-script/addPrice.js
+++ others-pages/bag/others-script/addPrice.js
@@ -7,38 +7,49 @@ function addPrice () {
 
         const clickedOnButtons = e.target;
 
-        if(clickedOnButtons.hasAttribute('type')) {
+        function addValue () {
 
-            function selectItems () {
-                const items = document.getElementsByClassName('product');
-                return items;
-            }
+            if(clickedOnButtons.hasAttribute('type')) {
     
-            let sumAllMultiplications = [];
-            let sum;
-    
-            function readAllItems () {
-    
-                const items = selectItems();
-    
-                for(let item of items) {
-                    const allH1 = item.querySelector('h1').innerText;
-                    const allNumItems = item.querySelector('.num-items').innerText;
-                    const resultMultiplications = allH1 * allNumItems;
-    
-                    sumAllMultiplications.push(resultMultiplications);
-    
-                    sum = sumAllMultiplications.reduce((total, valor) => total + valor, 0);
+                function selectItems () {
+                    const items = document.getElementsByClassName('product');
+                    return items;
                 }
-    
-                numPrice.innerText = sum.toFixed(2);
+        
+                let sumAllMultiplications = [];
+                let sum;
+        
+                function readAllItems () {
+        
+                    const items = selectItems();
+        
+                    for(let item of items) {
+                        const allH1 = item.querySelector('h1').innerText;
+                        const allNumItems = item.querySelector('.num-items').innerText;
+                        const resultMultiplications = allH1 * allNumItems;
+        
+                        sumAllMultiplications.push(resultMultiplications);
+        
+                        sum = sumAllMultiplications.reduce((total, valor) => total + valor, 0);
+                    }
+        
+                    numPrice.innerText = sum.toFixed(2);
+        
+                }
+        
+                readAllItems();
     
             }
-    
-            readAllItems();
 
         }
+
+        addValue();
+
     });
+
+    // window.addEventListener('load', () => {
+    //     teste();
+    // })
 
 }
 
