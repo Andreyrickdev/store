72a6fb3f7a2994e86894918ddc8a1472168a3e8b
Andreyrickdev <contato-andrey@outlook.com>
2023-11-04 16:46:45 -0300

    try execute this code

--- others-pages/bag/others-script/addPrice.js
+++ others-pages/bag/others-script/addPrice.js
@@ -3,6 +3,8 @@ function addPrice () {
     const button = document.getElementById('footer');
     const numPrice = button.getElementsByClassName('num-prices')[0];
 
+
+
     document.addEventListener('click', (e) => {
 
         const clickedOnButtons = e.target;
@@ -24,13 +26,16 @@ function addPrice () {
                     const items = selectItems();
         
                     for(let item of items) {
-                        const allH1 = item.querySelector('h1').innerText;
-                        const allNumItems = item.querySelector('.num-items').innerText;
-                        const resultMultiplications = allH1 * allNumItems;
-        
-                        sumAllMultiplications.push(resultMultiplications);
-        
-                        sum = sumAllMultiplications.reduce((total, valor) => total + valor, 0);
+                        try {
+                            const allH1 = item.querySelector('h1').innerText;
+                            const allNumItems = item.querySelector('.num-items').innerText;
+                            const resultMultiplications = allH1 * allNumItems;
+            
+                            sumAllMultiplications.push(resultMultiplications);
+            
+                            sum = sumAllMultiplications.reduce((total, valor) => total + valor, 0);
+                        } catch {}
+
                     }
         
                     numPrice.innerText = sum.toFixed(2);
