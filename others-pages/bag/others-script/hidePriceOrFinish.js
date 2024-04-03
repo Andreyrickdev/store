8e9ce8351185f9de9ade50f34a1a09643f6764b6
Andreyrickdev <contato-andrey@outlook.com>
2024-04-03 18:39:04 -0300

    removed space

--- others-pages/bag/others-script/hidePriceOrFinish.js
+++ others-pages/bag/others-script/hidePriceOrFinish.js
@@ -35,7 +35,6 @@ function hidePriceOrFinish () {
 
     function ifMouseOut () {
         document.addEventListener('mouseout', (e) => {
-    
             const price = selectPrice();
             const finish = selectFinish();
     
