a8a1769cf5f951083d9cb8e6df071c2ab459bb4d
Andreyrickdev <contato-andrey@outlook.com>
2024-04-17 19:57:32 -0300

    created function

--- others-pages/bag/others-script/createProduct.js
+++ others-pages/bag/others-script/createProduct.js
@@ -178,6 +178,10 @@
                         return p;
                     }
 
+                    function createInputAdd () {
+                        
+                    }
+
                 }
 
 
