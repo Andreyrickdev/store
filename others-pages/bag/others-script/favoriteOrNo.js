149f7f6ae74f750f92a7a45e89fc39b549608af0
Andreyrickdev <contato-andrey@outlook.com>
2024-05-21 19:18:48 -0300

    created function when you click on heart, add active class

---
+++ others-pages/bag/others-script/favoriteOrNo.js
@@ -0,0 +1,17 @@
+function hearts () {
+    const heart = document.getElementsByClassName('favorite');
+    return heart;
+}
+
+function eventOnHeart () {
+
+    const heart = hearts();
+
+    for (let fav of heart) {
+        fav.addEventListener('click', () => {
+            fav.classList.toggle('active');
+        });
+    }
+}
+
+eventOnHeart();
