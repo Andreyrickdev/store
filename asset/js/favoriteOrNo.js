273bdf787606e9ad788023f8c50f01fe65cf5654
Andreyrickdev <contato-andrey@outlook.com>
2024-05-23 14:51:31 -0300

    put favorite in localStorage

--- asset/js/favoriteOrNo.js
+++ asset/js/favoriteOrNo.js
@@ -7,9 +7,32 @@ function eventOnHeart () {
 
     const heart = hearts();
 
+    const favoritedProducts = [];
+
+    function getLastClass(element) {
+        const className = element.className;
+        const classes = className.split(' ');
+        return classes[1];
+    }
+
     for (let fav of heart) {
         fav.addEventListener('click', (e) => {
             fav.getElementsByTagName('li')[0].classList.toggle('active');
+            const yourProduct = fav.parentNode.parentNode;
+            const gettingTheLastOne = getLastClass(yourProduct);
+
+            if(fav.getElementsByTagName('li')[0].classList.contains('active')) {
+                favoritedProducts.push(gettingTheLastOne);
+                console.log(favoritedProducts);
+            } else {
+                const itemForRemove = favoritedProducts.indexOf(gettingTheLastOne);
+                if (itemForRemove !== -1) {
+                    favoritedProducts.splice(itemForRemove, 1);
+                }
+            }
+
+            const arrayToString = JSON.stringify(favoritedProducts);
+            localStorage.setItem("favoriteProducts", arrayToString);
         });
     }
 }
