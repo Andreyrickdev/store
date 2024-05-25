bac62811b26edc1927f8028bce28ae873d077a21
Andreyrickdev <contato-andrey@outlook.com>
2024-05-25 19:04:00 -0300

    linking the favorite from one page to the other page

--- others-pages/bag/others-script/favoriteOrNo.js
+++ others-pages/bag/others-script/favoriteOrNo.js
@@ -3,13 +3,58 @@ function hearts () {
     return heart;
 }
 
+function whenPageLoadPutActiveOnHeart () {
+    document.addEventListener('DOMContentLoaded', () => {
+
+        const getFavoritesInLocalStorage = localStorage.getItem('favoriteProducts');
+
+        const StringToObject = JSON.parse(getFavoritesInLocalStorage);
+
+        for(product of StringToObject) {
+            if(product == undefined) continue;
+
+            const getProduct = document.getElementsByClassName(product)[0];
+            if(!getProduct) continue;
+
+            const getFavorite = getProduct.getElementsByClassName('favorite')[0];
+            getFavorite.classList.add('active');
+        }
+
+    }) ;
+}
+whenPageLoadPutActiveOnHeart();
+
 function eventOnHeart () {
 
     const heart = hearts();
 
+    const getFavoriteProductsInLocalStorage = localStorage.getItem("favoriteProducts");
+    const stringToObject = JSON.parse(getFavoriteProductsInLocalStorage);
+    console.log(stringToObject);
+
+    function getLastClass (element) {
+        const className = element.className;
+        const classes = className.split(' ');
+        return classes[1];
+    }
+
     for (let fav of heart) {
         fav.addEventListener('click', () => {
             fav.classList.toggle('active');
+            const product = fav.parentNode.parentNode.parentNode;
+            const gettingTheLastOne = getLastClass(product);
+            const itemForRemove = stringToObject.indexOf(gettingTheLastOne);
+
+            if(fav.classList.contains('active')) {
+                stringToObject.push(gettingTheLastOne);
+                console.log(stringToObject);
+            } else {
+                stringToObject.splice(itemForRemove, 1);
+                console.log(stringToObject);
+            }
+
+            const arrayToString = JSON.stringify(stringToObject);
+            localStorage.setItem("favoriteProducts", arrayToString);
         });
     }
 }
