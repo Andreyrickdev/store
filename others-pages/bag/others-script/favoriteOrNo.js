f22a909f692ef8d4e657cc587a8763e11aa0b655
Andreyrickdev <contato-andrey@outlook.com>
2024-05-27 18:48:36 -0300

    checking if the array exists in localStorage

--- others-pages/bag/others-script/favoriteOrNo.js
+++ others-pages/bag/others-script/favoriteOrNo.js
@@ -8,16 +8,21 @@ function whenPageLoadPutActiveOnHeart () {
 
         const getFavoritesInLocalStorage = localStorage.getItem('favoriteProducts');
 
-        const StringToObject = JSON.parse(getFavoritesInLocalStorage);
+        let stringToObject = [];
+        if(getFavoritesInLocalStorage) {
+            stringToObject = JSON.parse(getFavoritesInLocalStorage);
+        }
 
-        for(product of StringToObject) {
-            if(product == undefined) continue;
-
-            const getProduct = document.getElementsByClassName(product)[0];
-            if(!getProduct) continue;
-
-            const getFavorite = getProduct.getElementsByClassName('favorite')[0];
-            getFavorite.classList.add('active');
+        if(Array.isArray(stringToObject)) {
+            for(product of stringToObject) {
+                if(product == undefined) continue;
+    
+                const getProduct = document.getElementsByClassName(product)[0];
+                if(!getProduct) continue;
+    
+                const getFavorite = getProduct.getElementsByClassName('favorite')[0];
+                getFavorite.classList.add('active');
+            }
         }
 
     }) ;
@@ -29,8 +34,10 @@ function eventOnHeart () {
     const heart = hearts();
 
     const getFavoriteProductsInLocalStorage = localStorage.getItem("favoriteProducts");
-    const stringToObject = JSON.parse(getFavoriteProductsInLocalStorage);
-    console.log(stringToObject);
+    let stringToObject = [];
+    if(getFavoriteProductsInLocalStorage) {
+        stringToObject = JSON.parse(getFavoriteProductsInLocalStorage);
+    }
 
     function getLastClass (element) {
         const className = element.className;
@@ -38,24 +45,26 @@ function eventOnHeart () {
         return classes[1];
     }
 
-    for (let fav of heart) {
-        fav.addEventListener('click', () => {
-            fav.classList.toggle('active');
-            const product = fav.parentNode.parentNode.parentNode;
-            const gettingTheLastOne = getLastClass(product);
-            const itemForRemove = stringToObject.indexOf(gettingTheLastOne);
-
-            if(fav.classList.contains('active')) {
-                stringToObject.push(gettingTheLastOne);
-                console.log(stringToObject);
-            } else {
-                stringToObject.splice(itemForRemove, 1);
-                console.log(stringToObject);
-            }
-
-            const arrayToString = JSON.stringify(stringToObject);
-            localStorage.setItem("favoriteProducts", arrayToString);
-        });
+    if(Array.isArray(stringToObject)) {
+        for (let fav of heart) {
+            fav.addEventListener('click', () => {
+                fav.classList.toggle('active');
+                const product = fav.parentNode.parentNode.parentNode;
+                const gettingTheLastOne = getLastClass(product);
+                const itemForRemove = stringToObject.indexOf(gettingTheLastOne);
+    
+                if(fav.classList.contains('active')) {
+                    stringToObject.push(gettingTheLastOne);
+                    console.log(stringToObject);
+                } else {
+                    stringToObject.splice(itemForRemove, 1);
+                    console.log(stringToObject);
+                }
+    
+                const arrayToString = JSON.stringify(stringToObject);
+                localStorage.setItem("favoriteProducts", arrayToString);
+            });
+        }
     }
 }
 
