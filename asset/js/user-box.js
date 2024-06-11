d286b68c148393cb22bf8d32cd8a7b59a73cc7f2
Andreyrickdev <contato-andrey@outlook.com>
2024-06-11 20:21:50 -0300

    adjusted element by id

--- asset/js/user-box.js
+++ asset/js/user-box.js
@@ -3,7 +3,7 @@ function userBox () {
     const button = document.getElementById('user');
     const box = document.getElementById('user-box');
 
-    const searchBox = document.getElementById('search-now');
+    const searchBox = document.getElementById('search-now-box');
     const shoppingBagBox = document.getElementById('shopping-bag-box');
 
     button.addEventListener('click', () => {
