a87c63a741b9a3c3c7c41a563769677940a5a87e
Andreyrickdev <contato-andrey@outlook.com>
2024-08-06 04:23:19 -0300

    set aria-expanded work

--- asset/js/search.js
+++ asset/js/search.js
@@ -6,8 +6,16 @@ function functSearch () {
     const shoppingBagBox = document.getElementById('shopping-bag-box');
     const userBox = document.getElementById('user-box');
 
+    const userButton = document.getElementById('user');
+    const bagButton = document.getElementById('shopping-bag');
 
     search.addEventListener('click', () => {
+        let ariaExpanded = search.getAttribute('aria-expanded') === 'true';
+        search.setAttribute('aria-expanded', !ariaExpanded);
+
+        userButton.setAttribute('aria-expanded', false);
+        bagButton.setAttribute('aria-expanded', false);
+
         shoppingBagBox.classList.remove('active');
         userBox.classList.remove('active');
         inputSearch.classList.toggle('active');
